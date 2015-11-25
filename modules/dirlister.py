7d48523900ad17b3577e49b09340a4b49872308b
tanyimin <mrtan792.yahoo.com>
2015-11-25 14:42:16 +0800

    Adding new modules

---
+++ modules/dirlister.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import os
+def run(**args):
+
+	print "[*] In dirlister module"
+	files = os.listdir(".")
+
+
+	return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+import os
+
+def run(**args):
+	print "[*] In enviroment module"
+
+	run str(os.environ)
